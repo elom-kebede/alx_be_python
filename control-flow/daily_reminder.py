11dcd0bd1a3578537ba8a9e19935acfd460b4e53
elom-kebede <kebedeelias30@gmail.com>
2024-12-20 04:00:22 -0800

    daily_reminder first commit

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,16 @@
+task = input("Enter your task:")
+priority = input("Priority (high/medium/low):")
+time =input("Is it time-bound? (yes/no):")
+match priority:
+    case "high":
+        reminder = f" '{task}' is a high priority task "
+    case "medium":
+        reminder = f"'{task}' is a medium priority task "
+    case "low":
+        reminder = f" '{task}' is a low priority task. "
+if time == "yes":
+    reminder+="that requires immediate attention today!"
+    print(f"Reminder: {reminder}")
+else:
+    reminder+="Consider completing it at your convenience."
+    print(f"Note: {reminder}")
