beb385c83efa069dd9fe281f007e8f9046a5e73b
elom-kebede <kebedeelias30@gmail.com>
2024-12-20 03:04:08 -0800

    changing format

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,3 @@
-x = int(input("Enter a number to see its multiplication table:"))
+x = int(input("Enter a number to see its multiplication table: "))
 for y in range(1,11):
-    res = x * y
-    print(f"{x} * {y} = {res}")
+   print(f"{x} * {y} = {x*y}")
