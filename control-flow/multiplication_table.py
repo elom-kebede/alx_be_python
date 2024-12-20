b9bb781c77eaa62b568310c2767f50c377ddc62b
elom-kebede <kebedeelias30@gmail.com>
2024-12-20 04:24:23 -0800

    change var of x with number

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,3 +1,3 @@
-x = int(input("Enter a number to see its multiplication table: "))
+number = int(input("Enter a number to see its multiplication table: "))
 for y in range(1,11):
-   print(f"{x} * {y} = {x*y}")
+   print(f"{number} * {y} = {number*y}")
