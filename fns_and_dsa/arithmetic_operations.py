76303380c00879c8b58e39906bc9e258818825ca
elom-kebede <kebedeelias30@gmail.com>
2025-01-13 23:17:33 -0800

    giving space

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
         case "multiply":
             return num1*num2
         case "divide":
-            if num2==0:
+            if num2 == 0:
                 return "Error: Division by zero is undefined."
             return num1/num2
         case _:
