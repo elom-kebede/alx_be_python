933fe62a61125673055287151fcd77192a594bd6
elom-kebede <kebedeelias30@gmail.com>
2025-01-13 23:20:58 -0800

    adding elif statment

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return "Error: Division by zero is undefined."
-            else:
+            elif num2 != 0:
                 return num1/num2
         case _:
             return "Error: Unsupported operation."
